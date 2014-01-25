03277d432eb8362947c5ecb1119a814fff3f5e19
Ray Nicholus <github.com@garstasio.com>
2014-01-24 22:44:37 -0600

    chore($setup): Rules
    - JSHint rules
    - code tidying
    - move dev env to Web Storm (for now)
    - better URL generator for requests on server

--- server/server.js
+++ server/server.js
@@ -1,20 +1,31 @@
+/* jshint node:true */
+'use strict';
 var express = require('express'),
-    fs = require('fs'),
-    githubOauthData = require("./github_oauth_data.json"),
+    oauth_data = require('./github_oauth_data.json'),
+    uuid = require('node-uuid'),
+    querystring = require('querystring'),
     app = express();
 
-app.use(express.static("./client/"));
+app.use(express.static('./client/'));
 
 app.get('/github/getauth', function(req, res) {
-    console.log("Requesting autorization from Github...");
-    res.redirect("https://github.com/login/oauth/authorize?client_id=" + githubOauthData.clientId +
-    "&scope=repo&state=" + Math.random());
+    var authParams = {
+            client_id: oauth_data.client_id,
+            scope: 'repo',
+            state: uuid.v1()
+        },
+        authUrl = 'https://github.com/login/oauth/authorize?' +
+            querystring.stringify(authParams);
+
+    console.log('Requesting autorization from Github...');
+
+    res.redirect(authUrl);
 });
 
 app.get('/github/gotauth', function(req, res) {
-    console.log("Got authorization from Github.");
-    res.send("Authorized");
+    console.log('Got authorization from Github.');
+    res.send('Authorized');
 });
 
-app.listen(process.env.PORT);
-console.log('Express server started on port %s', process.env.PORT);
+app.listen(9000);
+console.log('Express server started on port %s',9000);
