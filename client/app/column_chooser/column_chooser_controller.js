f8da724d43c96d68b5233c47f5a8c19133b9f9c4
Ray Nicholus <github.com@garstasio.com>
2014-02-24 22:20:45 -0600

    feat($labels): column chooser modal UI

--- client/app/column_chooser/column_chooser_controller.js
+++ client/app/column_chooser/column_chooser_controller.js
@@ -1,2 +1,6 @@
 Nimble.ColumnChooserController = Ember.ObjectController.extend({
+    save_columns: function(names) {
+        //TODO add/update labels in repo
+        this.transitionToRoute("index");
+    }
 });
--- client/app/column_chooser/column_chooser_view.js
+++ client/app/column_chooser/column_chooser_view.js
@@ -11,6 +11,25 @@ Nimble.ColumnChooserView = Ember.View.extend({
             $("#columns-modal").modal("hide").on("hidden.bs.modal", function() {
                 //TODO
             }.bind(this));
+        },
+
+        add_column: function() {
+            var $list_input = this.$(".list-input:first").clone();
+
+            $list_input.find("input").val("");
+            this.$(".list-input:last").after($list_input);
+        },
+
+        save_columns: function() {
+            var names = [];
+
+            this.$(".list-input INPUT[type='text']").each(function() {
+                var name = $(this).val().trim();
+                name && names.push(name);
+            });
+
+            this.controller.send("close_modal");
+            this.controller.save_columns(names);
         }
     }
 });
