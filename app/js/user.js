c905becce6da8a2a11d87970169935e3dc261482
Ray Nicholus <github.com@garstasio.com>
2014-03-30 22:33:59 -0500

    test($angular): fix dir structure & server paths #7

---
+++ app/js/user.js
@@ -0,0 +1,78 @@
+nimbleModule.controller("userController", ["$scope", "user",
+    function($scope, user) {
+        angular.extend($scope, {
+            loggedIn: user.isLoggedIn(),
+
+            login: user.login,
+
+            logout: function() {
+                user.logout().then(function() {
+                    $scope.loggedIn = false;
+                });
+            }
+        });
+
+        if ($scope.loggedIn) {
+            user.getInfo().then(function(info) {
+                $scope.avatarUrl = info.avatar_url;
+            });
+        }
+
+    }])
+
+    .directive("nimbleUser", function() {
+        return {
+            restrict: "E",
+            templateUrl: "/partials/user.html"
+        };
+    })
+
+    .factory("user", ["$http", "$q", function($http, $q) {
+        var tokenUrl = "/github/token",
+            token = document.cookie.replace(
+                /(?:(?:^|.*;\s*)github_token\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+
+        return {
+            isLoggedIn: function() {
+                return !!token;
+            },
+
+            login: function() {
+                window.location = tokenUrl;
+            },
+
+            logout: function() {
+                var deferred = $q.defer();
+
+                $http.delete(tokenUrl)
+                    .success(function() {
+                        token = null;
+                        deferred.resolve();
+                    })
+                    .error(function(data, status) {
+                        console.error("Could not log out user due to server error.");
+                        deferred.reject({status: status});
+                    });
+
+                return deferred.promise;
+            },
+
+            // TODO Move most of this to a new "github API" service
+            getInfo: function() {
+                var host = "https://api.github.com",
+                    deferred = $q.defer();
+
+                $http.get(host + "/user", {
+                    params: {
+                        access_token: token
+                    }
+                }).success(function(data) {
+                    deferred.resolve(data);
+                }).error(function(data, status) {
+                    deferred.reject({status: status});
+                });
+
+                return deferred.promise;
+            }
+        };
+    }]);
