b54cab0fa3e04969244b229983b6f72e51b6f417
Ray Nicholus <github.com@garstasio.com>
2014-03-30 21:58:23 -0500

    test($angular): complete user controller tests #7
    Also run tests on travis

--- client/grunt_tasks/karma.js
+++ client/grunt_tasks/karma.js
@@ -1,7 +1,32 @@
 /* jshint node:true */
 /* globals module */
 module.exports = {
-    unit: {
-        configFile: "config/karma.conf.js"
+    options: {
+        autoWatch : false,
+
+        basePath : ".",
+
+        files : [
+            "app/lib/*.js",
+            "test/lib/angular/*",
+            "app/js/**/*.js",
+            "test/unit/**/*.js"
+        ],
+
+        frameworks: ["jasmine"],
+
+        singleRun: true,
+
+        plugins : [
+            "karma-chrome-launcher",
+            "karma-firefox-launcher",
+            "karma-jasmine"
+        ]
+    },
+    dev: {
+        browsers: ["Chrome"]
+    },
+    travis: {
+        browsers: ["Firefox"]
     }
 };
