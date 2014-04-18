d0ae9f036ce367b9c7e0bd063bfea3b8cfbd5356
Ray Nicholus <github.com@garstasio.com>
2014-04-17 22:47:00 -0500

    feat($repo-chooser): "Please Wait" indicator in modal on repo list load #7

--- app/src/app.js
+++ app/src/app.js
@@ -1,2 +1,7 @@
 /*jshint -W079 */
-var nimbleModule = angular.module("nimble", ["ui.bootstrap"]);
+var nimbleModule = angular.module("nimble", [
+    "ui.bootstrap",
+    "ngAnimate",
+    "ajoslin.promise-tracker",
+    "cgBusy"
+]);
