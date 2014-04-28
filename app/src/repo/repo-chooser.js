6b4530d2d1b5a309ad907bdc0086eba868fbe173
Ray Nicholus <github.com@garstasio.com>
2014-04-27 22:05:56 -0500

    chore(repo-chooser.js): prevent minification issues #7

--- app/src/repo/repo-chooser.js
+++ app/src/repo/repo-chooser.js
@@ -1,40 +1,41 @@
-// TODO fix this to avoid minification issues
-var repoChooserInstanceController = function($scope, $modalInstance, repos, promiseTracker, systemAlert) {
-    function sort(reposArray) {
-        reposArray.sort(function(a, b) {
-            var aName = a.full_name.toLowerCase(),
-                bName = b.full_name.toLowerCase();
+var repoChooserInstanceController = ["$scope", "$modalInstance", "repos", "promiseTracker", "systemAlert",
+    function($scope, $modalInstance, repos, promiseTracker, systemAlert) {
 
-            if (aName < bName) {
-                return -1;
-            }
-            if (aName > bName) {
-                return 1;
-            }
-            return 0;
-        });
+        function sort(reposArray) {
+            reposArray.sort(function(a, b) {
+                var aName = a.full_name.toLowerCase(),
+                    bName = b.full_name.toLowerCase();
 
-        return reposArray;
-    }
-
-    $scope.close = $modalInstance.dismiss;
-    $scope.ok = $modalInstance.close;
-
-    $scope.typedRepos = [];
-
-    promiseTracker("getrepos").addPromise(repos.get().then(
-        function success(groupedRepos) {
-            groupedRepos.forEach(function(reposGroup) {
-                sort(reposGroup.repos);
-                $scope.typedRepos.push(reposGroup);
+                if (aName < bName) {
+                    return -1;
+                }
+                if (aName > bName) {
+                    return 1;
+                }
+                return 0;
             });
-        },
 
-        function failure() {
-            systemAlert.show("error", "Problem retrieving repository list");
+            return reposArray;
         }
-    ));
-};
+
+        $scope.close = $modalInstance.dismiss;
+        $scope.ok = $modalInstance.close;
+
+        $scope.typedRepos = [];
+
+        promiseTracker("getrepos").addPromise(repos.get().then(
+            function success(groupedRepos) {
+                groupedRepos.forEach(function(reposGroup) {
+                    sort(reposGroup.repos);
+                    $scope.typedRepos.push(reposGroup);
+                });
+            },
+
+            function failure() {
+                systemAlert.show("error", "Problem retrieving repository list");
+            }
+        ));
+    }];
 
 nimbleModule.controller("repoChooserController", ["$scope", "$modal", "user",
     function($scope, $modal, user) {
