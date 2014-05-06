4af7bfddf287c716ced7cdefa5d78f1afc85c14f
Ray Nicholus <github.com@garstasio.com>
2014-05-05 23:11:04 -0500

    test($setup-columns): more column update tests #7

--- test/unit/columns-service-spec.js
+++ test/unit/columns-service-spec.js
@@ -51,7 +51,6 @@ describe("Columns service", function() {
         ]);
     });
 
-    // TODO test removed columns
     describe("update", function() {
         it("does nothing if no changes exist", function() {
             this.columns.current = [
@@ -66,12 +65,11 @@ describe("Columns service", function() {
             this.$rootScope.$digest();
         });
 
-        // TODO test that proper github svc methods are called
-        it("handles a new & changed columns correctly", function() {
+        it("handles new & changed columns correctly", function() {
             var proposedColumns = [
                     {name: "2", label: "0 - 1"},
-                    {name: "5", label: "0 - 2"},
-                    {name: "3", label: "0 - 3"},
+                    {name: "5", label: "1 - 2"},
+                    {name: "3", label: "2 - 3"},
                     {name: "foo"}
                 ],
                 expectedColumns = [
@@ -83,12 +81,73 @@ describe("Columns service", function() {
 
             this.columns.current = [
                 {name: "1", label: "0 - 1"},
-                {name: "2", label: "0 - 2"},
-                {name: "3", label: "0 - 3"}
+                {name: "2", label: "1 - 2"},
+                {name: "3", label: "2 - 3"}
             ];
+
+            this.user.selectedRepoName = "garstasio/foobar";
+            spyOn(this.github, "updateLabel").and.returnValue(this.$q.defer().promise);
+            spyOn(this.github, "createLabel").and.returnValue(this.$q.defer().promise);
 
             this.columns.update(proposedColumns);
 
+            // updates
+            expect(this.github.updateLabel).toHaveBeenCalledWith("garstasio/foobar", {
+                oldLabel: "0 - 1",
+                newLabel: "0 - 2"
+            });
+            expect(this.github.updateLabel).toHaveBeenCalledWith("garstasio/foobar", {
+                oldLabel: "1 - 2",
+                newLabel: "1 - 5"
+            });
+            expect(this.github.updateLabel).not.toHaveBeenCalledWith("garstasio/foobar", {
+                oldLabel: "2 - 3",
+                newLabel: "2 - 3"
+            });
+
+            // adds
+            expect(this.github.createLabel).toHaveBeenCalledWith("garstasio/foobar", "3 - foo");
+
+            // new columns
+            expect(this.columns.current).toEqual(expectedColumns);
+        });
+
+        it("handles new & deleted columns correctly", function() {
+            var proposedColumns = [
+                    {name: "1", label: "0 - 1"},
+                    {name: "foo", label: "1 - 2"}
+                ],
+                expectedColumns = [
+                    {name: "1", label: "0 - 1"},
+                    {name: "foo", label: "1 - foo"}
+                ];
+
+            this.columns.current = [
+                {name: "1", label: "0 - 1"},
+                {name: "2", label: "1 - 2"},
+                {name: "3", label: "2 - 3"}
+            ];
+
+            this.user.selectedRepoName = "garstasio/foobar";
+            spyOn(this.github, "updateLabel").and.returnValue(this.$q.defer().promise);
+            spyOn(this.github, "deleteLabel").and.returnValue(this.$q.defer().promise);
+
+            this.columns.update(proposedColumns);
+
+            // updates
+            expect(this.github.updateLabel).not.toHaveBeenCalledWith("garstasio/foobar", {
+                oldLabel: "0 - 1",
+                newLabel: "0 - 1"
+            });
+            expect(this.github.updateLabel).toHaveBeenCalledWith("garstasio/foobar", {
+                oldLabel: "1 - 2",
+                newLabel: "1 - foo"
+            });
+
+            // deletes
+            expect(this.github.deleteLabel).toHaveBeenCalledWith("garstasio/foobar", "2 - 3");
+
+            // new columns
             expect(this.columns.current).toEqual(expectedColumns);
         });
     });
